98c6b6e6189621711c739cc4746ea1b17e7aa93e
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-07-16 13:23:15 +0200

    Bug on image-pipe fixed and animations added

--- src/app/heroes/pipes/image.pipe.ts
+++ src/app/heroes/pipes/image.pipe.ts
@@ -8,7 +8,10 @@ export class ImagePipe implements PipeTransform {
 
   transform(hero: Hero): string {
 
-    if(!hero.id && !hero.alt_img){
+    console.log("heroid", hero.id);
+    console.log("heroaltimg", hero.alt_img);
+
+    if(hero.id==undefined && hero.alt_img== " "){
       return 'assets/no-image.png';
     } else if (hero.alt_img) {
       return hero.alt_img;
